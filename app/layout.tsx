9ae41c9deaf3b3f8a86fcbfd2fbd6781c18fa199
JunelAmelon <junelamelon92@gmail.com>
2025-07-12 22:23:07 +0100

    upadte project

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,50 @@ const workSans = Work_Sans({
 });
 
 export const metadata: Metadata = {
-  title: 'SecureAcompte',
-  description: 'Sécurisez vos acomptes de travaux en toute confiance',
+  title: 'Secure Acompte Travaux | La solution par AXIMOTRAVO',
+  description: 'Secure Acompte Travaux est la solution innovante d’AXIMOTRAVO pour sécuriser les acomptes de travaux. Automatisez la gestion des paiements, sécurisez les transactions, et gagnez la confiance de vos clients.',
+  keywords: [
+    'acomptes travaux',
+    'sécurisation paiements',
+    'gestion acomptes',
+    'sécuriser acompte client',
+    'Secure Acompte',
+    'solution AXIMOTRAVO',
+    'sécurité des paiements BTP',
+    'plateforme de gestion de paiements',
+    'acompte chantier',
+    'sécurité financière travaux'
+  ],
+  authors: [{ name: 'AXIMOTRAVO', url: 'https://secureacomptetravaux.com' }],
+  creator: 'AXIMOTRAVO',
+  publisher: 'AXIMOTRAVO',
+  applicationName: 'Secure Acompte Travaux',
+  robots: 'index, follow',
+  metadataBase: new URL('https://secureacomptetravaux.com'),
+  openGraph: {
+    title: 'Secure Acompte Travaux | La solution sécurisée pour vos paiements BTP',
+    description: 'La plateforme d’AXIMOTRAVO pour une gestion simplifiée, fiable et sécurisée des acomptes dans le secteur du bâtiment et des travaux.',
+    url: 'https://secureacomptetravaux.com',
+    siteName: 'Secure Acompte Travaux',
+    locale: 'fr_FR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://secureacomptetravaux.com/img/preview.jpg', // Remplace par ton image de partage
+        width: 1200,
+        height: 630,
+        alt: 'Illustration Secure Acompte Travaux'
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Secure Acompte Travaux | Par AXIMOTRAVO',
+    description: 'Solution de sécurisation des acomptes dans le secteur des travaux. Simplifiez la gestion et gagnez en confiance.',
+    images: ['https://secureacomptetravaux.com/img/preview.jpg']
+  }
 };
+
 
 export default function RootLayout({
   children,
